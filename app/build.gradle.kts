8f5952d9a49fa1b1dfa515b872f4d20bffc9c33c
aeldy <noelnoskill@gmail.com>
2026-03-16 11:55:52 +0700

    Update compile and target SDK versions to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,60 +1,68 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.android.application)
+  alias(libs.plugins.android.application)
 }
 
 android {
-    namespace = "com.shinigami.client"
-    compileSdk = 35
+  namespace = "com.shinigami.client"
+  compileSdk = 36
 
-    defaultConfig {
-        applicationId = "com.shinigami.client"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.shinigami.client"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0-github"
 
-        androidResources {
-            localeFilters.addAll(listOf("en", "in"))
-        }
+    androidResources {
+      localeFilters.addAll(listOf("en", "in"))
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("debug")
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
+      signingConfig = signingConfigs.getByName("debug")
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+
+  // pengganti kotlinOptions untuk agp 9.0+
+  kotlin {
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_21)
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-    }
+  }
+
+  buildFeatures {
+    viewBinding = true
+    buildConfig = true
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.fragment.ktx)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.viewmodel.ktx)
+  implementation(libs.androidx.activity.ktx)
+  implementation(libs.androidx.fragment.ktx)
 
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.okhttp)
-    implementation(libs.androidx.startup)
+  implementation(libs.kotlinx.coroutines.android)
+  implementation(libs.okhttp)
+  implementation(libs.androidx.startup)
 
-    implementation(libs.androidx.swiperefreshlayout)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.material)
+  implementation(libs.androidx.swiperefreshlayout)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.material)
 }
